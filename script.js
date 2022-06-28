be1befd1b35c3147e9896a33bf149ece02fb0ece
MJ_Shelton <mjshelton12@comcast.net>
2022-06-27 20:43:41 -0700

    Small wording fixes in text elements

--- script.js
+++ script.js
@@ -23,28 +23,28 @@ while (promptLengthNum < 8 || promptLengthNum > 128) {
   }
 }
 
-if (window.confirm("Click Ok if you would like to include special characters (!@#$%^&*()) or Cancel if you don't.")){
-  promptSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-  } else {
-  promptSpecial = []
-}
-
 if (window.confirm("Click Ok if you would like to include numbers or Cancel if you don't.")){
   promptNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   } else {
   promptNumbers = []
 }
 
-if (window.confirm("Click Ok if you would like to include uppercase letters or Cancel if you don't.")){
+if (window.confirm("Got it! Click Ok if you would like to include uppercase letters or Cancel if you don't.")){
   promptUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
   } else {
   promptUpper = []
 }
 
-if (window.confirm("Click Ok if you would like to include lowercase letters or Cancel if you don't.")){
+if (window.confirm("Ok! Click Ok if you would like to include lowercase letters or Cancel if you don't.")){
   promptLower= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
   } else {
   promptLower = []
+}
+
+if (window.confirm("We got you! Click Ok if you would like to include special characters (!@#$%^&*()) or Cancel if you don't.")){
+  promptSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+  } else {
+  promptSpecial = []
 }
 
 putItAllTogether = promptSpecial.concat(promptNumbers, promptUpper, promptLower)
